370f564d7d279689947ea7aca62faaa02f085c79
Paule <44635962+V3lop5@users.noreply.github.com>
2021-11-05 14:43:19 +0100

    Added script to run all

--- run-all.sh
+++ run-all.sh
@@ -2,19 +2,27 @@
 
 co2_arr=(0.75 0.8 0.85 0.9 0.95 1)
 
-create_notebook() {
-  cp "$1" "$2"
-  sed -i "s/\"CO2_reductionTarget = .*\"/\"CO2_reductionTarget = $3\"/g" "$2"
-}
+temp="Temp.ipynb"
 
+# Create folder for generated notebooks
+notebook_folder="./notebooks"
+mkdir -p "$notebook_folder"
 
+# Generate notebooks
 for i in "${co2_arr[@]}"
 do
 	echo "> Processing co2 reduction $i"
-	notebook="notebooks/CO2_reduction_$((100 * i))_percent.ipynb"
-	echo "> Creating notebook $notebook ..."
-	create_notebook "Multi-regional_Energy_System_Workflow.ipynb" "$notebook" "$i"
-	echo "> Executing notebook $notebook ..."
-	jupyter nbconvert --to notebook --execute "$notebook"
-	echo "> Done"
+	echo "> Creating temp notebook ..."
+	cp "Multi-regional_Energy_System_Workflow.ipynb" "$temp"
+  sed -i "s/\"CO2_reductionTarget = .*\"/\"CO2_reductionTarget = $i\"/g" "$temp"
+
+  echo "> Run temp notebook ..."
+  jupyter nbconvert --to notebook --inplace --execute "$temp"
+
+  echo "> Copy temp notebook to /notebooks"
+	notebook="$notebook_folder/CO2_reduction_$i.ipynb"
+	cp "$temp" "$notebook"
 done
+
+rm "$temp"
+echo "> Done "
