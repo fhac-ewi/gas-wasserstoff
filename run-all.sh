4e623058ad4de1f6f3b44094ecee2ba0e5ef7f4d
Paule <44635962+V3lop5@users.noreply.github.com>
2022-01-06 20:59:32 +0100

    Updated simulation

--- run-all.sh
+++ run-all.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-co2_arr=(0.75 0.8 0.85 0.9 0.95 1)
-
-temp="Temp.ipynb"
+co2_arr=(0.0 0.75 0.8 0.85 0.9 0.95 1)
 
 
 # Clear results, notebooks and images folder
@@ -20,16 +18,17 @@ for i in "${co2_arr[@]}"
 do
 	echo "> Processing co2 reduction $i"
 	echo "> Creating temp notebook ..."
+	temp="CO2_reduction_$i.ipynb"
 	cp "Multi-regional_Energy_System_Workflow.ipynb" "$temp"
   sed -i "s/\"CO2_reductionTarget = .*\"/\"CO2_reductionTarget = $i\"/g" "$temp"
 
-  echo "> Run temp notebook ..."
+  echo "> Run notebook $temp ..."
   jupyter nbconvert --to notebook --inplace --execute "$temp"
 
   echo "> Copy temp notebook to /notebooks"
-	notebook="$notebook_folder/CO2_reduction_$i.ipynb"
-	cp "$temp" "$notebook"
+	final_notebook="$notebook_folder/CO2_reduction_$i.ipynb"
+	cp "$temp" "$final_notebook"
+	rm "$temp"
 done
 
-rm "$temp"
 echo "> Done "
